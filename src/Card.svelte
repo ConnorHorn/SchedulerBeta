f2306dadb07d6135532895db681e90ba2e6930a0
ConnorHorn <connorhornet@gmail.com>
2022-07-21 01:33:21 -0400

    ughh

--- src/Card.svelte
+++ src/Card.svelte
@@ -4,21 +4,28 @@
     export let vote = "";
     let evalBase = "data.Artists."+artist+".";
     let genres = ["temp1","temp2","temp3"];
+    let prompts = ["Cooler"]
 </script>
 
 <div class="card bg-base-300 shadow-xl h-fixed w-fixed h-full w-full z-10">
     <figure class="px-10 pt-10 h-1/2">
         <img src={eval(evalBase+"ImageURL")} alt="Artist Logo" class="rounded-xl" />
     </figure>
-    <div class="card-body items-center text-center space-y-3">
+    <div class="card-body items-center text-center space-y-3 ">
         <h2 class="card-title text-6xl">{eval(evalBase+"Name")}</h2>
         <div class="space-x-1">
             {#each genres as genre}
             <div class="badge badge-accent">{genre}</div>
                 {/each}
         </div>
-        <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions pt-12">
+            <button class="btn btn-primary btn-square w-72 h-36 relative">
+
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-20" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 11l7-7 7 7M5 19l7-7 7 7" />
+                </svg>
+
+            </button>
         </div>
     </div>
 </div>
--- src/Eval.svelte
+++ src/Eval.svelte
@@ -3,13 +3,38 @@
 import Card from "./Card.svelte";
 
 let artist = "Illenium"
+let cardFlicker = true;
+function temp(){
+    artist = "Galantis"
+}
+$: {
+    updateCards(artist)
+}
+
+function updateCards(){
+    cardFlicker = false;
+    setTimeout(function() {
+        cardFlicker = true;
+    }, 1);
+
+}
 
 </script>
 
 
-<div class="w-1/3 h-4/5 fixed ml-40 mt-8 z-10">
-<Card {artist} vote = {"negative"} />
+{cardFlicker}
+<button on:click = {temp} class="btn z-30 fixed">Button</button>
+{#if cardFlicker}
+<div class="grid grid-cols-2 grid-rows-1 gap-20 w-full h-full fixed items-center -mt-12">
+    <div class="box w-2/3 h-3/4 justify-self-end">
+        <Card {artist} vote = {"left"} />
+    </div>
+    <div class="box w-2/3 h-3/4 justify-self-start">
+        <Card {artist} vote = {"right"} />
+    </div>
 </div>
+    {/if}
+
 
 
 
